536eb2546e69149498c5d4165a99b48773bd9a1a
milaq <micha.laqua@gmail.com>
2017-02-24 02:03:17 +0100

    initial commit

---
+++ cherry-picks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function check_clean {
+  if [ -e *.patch ]
+  then
+    rm *.patch
+  fi
+  if [ -e ".git/rebase-apply" ]
+  then
+    git am --abort
+    exit 1
+  elif [ -e ".git/CHERRY_PICK_HEAD" ]
+  then
+    git cherry-pick --abort
+    exit 1
+  fi
+}
+
+function apply {
+  echo "Getting: $1"
+  wget -q https://raw.githubusercontent.com/milaq/rockbox-patches/master/patches/$1.patch
+  git am $1.patch
+  echo ""
+  check_clean
+}
+
+#################################################################
+
+# separators: modify 'auto' to draw only the title line
+apply separators
